3ec21662ddd70cda285b55444711140d01c19e0d
Nathy-tech <mannathy5@gmail.com>
2024-12-09 02:19:59 -0800

    model setup

--- models/user.js
+++ models/user.js
@@ -1,1 +1,35 @@
-ECHO is on.
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// Define user schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  },
+});
+
+// Hash password before saving the user
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+// Method to check password validity
+userSchema.methods.isValidPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
