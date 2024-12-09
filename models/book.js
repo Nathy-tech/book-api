21700b1e90d3ce7ecbfaa9d1412b660b646eeabd
Nathy-tech <mannathy5@gmail.com>
2024-12-09 02:20:51 -0800

    setting up the book model

--- models/book.js
+++ models/book.js
@@ -1,1 +1,19 @@
-ECHO is on.
+const mongoose = require('mongoose');
+
+// Define book schema
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Book', bookSchema);
