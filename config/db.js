4ced1142bfe529e9d19da7297d8935f04fb79780
Nathy-tech <mannathy5@gmail.com>
2024-12-09 02:18:23 -0800

    database setup

--- config/db.js
+++ config/db.js
@@ -1,1 +1,17 @@
-ECHO is on.
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://localhost:27017/book-api', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1); // Exit process with failure
+  }
+};
+
+module.exports = connectDB;
+
